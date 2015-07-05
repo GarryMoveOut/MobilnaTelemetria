792a010061071f9a95824892c53e11d9d7d3e32d
Damian Rychliński <drychlinski@outlook.com>
2015-07-05 19:39:43 +0200

    Utworzenie BluetoothFragment oraz widoku

---
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -0,0 +1,15 @@
+package pl.rychlinski.damian.mobilnatelemetria;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BluetoothFragment extends Fragment {
+
+
+}
