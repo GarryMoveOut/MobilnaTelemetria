cc28e3372012738b2c60f310835d903d4fe462e4
Damian Rychliński <drychlinski@outlook.com>
2015-07-05 22:39:35 +0200

    BluetoothFragment: sendMessage

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -4,6 +4,8 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -29,6 +31,7 @@ public class BluetoothFragment extends Fragment {
     private UslugaBluetooth mChatService = null;
     private ArrayAdapter<String> mConversationArrayAdapter;
     private StringBuffer mOutStringBuffer;
+    private String mConnectedDeviceName = null;
 
     // Layout Views
     private ListView mConversationView;
@@ -136,4 +139,57 @@ public class BluetoothFragment extends Fragment {
             mOutEditText.setText(mOutStringBuffer);
         }
     }
+
+    /**
+     * The Handler that gets information back from the BluetoothChatService
+     */
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            FragmentActivity activity = getActivity();
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case UslugaBluetooth.STATE_CONNECTED:
+                            setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
+                            mConversationArrayAdapter.clear();
+                            break;
+                        case UslugaBluetooth.STATE_CONNECTING:
+                            setStatus(R.string.title_connecting);
+                            break;
+                        case UslugaBluetooth.STATE_LISTEN:
+                        case UslugaBluetooth.STATE_NONE:
+                            setStatus(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    if (null != activity) {
+                        Toast.makeText(activity, "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    if (null != activity) {
+                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    };
 }
