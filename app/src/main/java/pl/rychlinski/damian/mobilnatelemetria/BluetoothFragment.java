4666f96ab5692a3ef03395eb4d59d8e260e0f361
Damian Rychliński <drychlinski@outlook.com>
2015-07-05 21:58:07 +0200

    BluetoothFragment:onCreate - sprawdzanie czy jest bluetooth

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -1,15 +1,34 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class BluetoothFragment extends Fragment {
+    private static final String TAG = "BluetoothFragment";
+    
+    private BluetoothAdapter mBluetoothAdapter = null;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        // Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            FragmentActivity activity = getActivity();
+            Toast.makeText(activity, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            activity.finish();
+        }
+    }
 }
