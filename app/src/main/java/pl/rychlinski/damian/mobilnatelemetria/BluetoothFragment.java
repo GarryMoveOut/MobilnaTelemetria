5ad906c4ae9d2f905237130e8eea210e137e399d
Damian Rychliński <drychlinski@outlook.com>
2015-07-05 22:04:31 +0200

    BluetoothFragment:onStart

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -1,21 +1,23 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 
 public class BluetoothFragment extends Fragment {
     private static final String TAG = "BluetoothFragment";
-    
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+
     private BluetoothAdapter mBluetoothAdapter = null;
+    private UslugaBluetooth mChatService = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,20 @@ public class BluetoothFragment extends Fragment {
             FragmentActivity activity = getActivity();
             Toast.makeText(activity, "Bluetooth is not available", Toast.LENGTH_LONG).show();
             activity.finish();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else if (mChatService == null) {
+            setupChat();
         }
     }
 }
