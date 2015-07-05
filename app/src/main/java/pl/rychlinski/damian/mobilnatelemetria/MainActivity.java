8dc9ebdfc3f770b9ace315e7029226f2b2dd03e0
Damian Rychliński <drychlinski@outlook.com>
2015-07-05 19:21:59 +0200

    MainFrame: dodanie fragmentu, poprawienie ActionBar (deprecated)

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/MainActivity.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/MainActivity.java
@@ -1,17 +1,22 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
