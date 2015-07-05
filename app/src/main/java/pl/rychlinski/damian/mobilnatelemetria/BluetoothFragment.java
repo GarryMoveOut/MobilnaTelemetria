a1a08abd058f9fe1d7f478b400aae6dedf0c5e03
Damian Rychliński <drychlinski@outlook.com>
2015-07-05 22:31:14 +0200

    BluetoothFragment: sendMessage

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -112,4 +112,28 @@ public class BluetoothFragment extends Fragment {
             return true;
         }
     };
+
+    /**
+     * Sends a message.
+     *
+     * @param message A string of text to send.
+     */
+    private void sendMessage(String message) {
+        // Check that we're actually connected before trying anything
+        if (mChatService.getState() != UslugaBluetooth.STATE_CONNECTED) {
+            Toast.makeText(getActivity(), R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+
+            // Reset out string buffer to zero and clear the edit text field
+            mOutStringBuffer.setLength(0);
+            mOutEditText.setText(mOutStringBuffer);
+        }
+    }
 }
