8e6b9edcae9c84e2bbef23ef0582778c0d693bc4
Damian Rychliński <drychlinski@outlook.com>
2015-07-06 12:10:40 +0200

    Utworzenie klasy DeviceListActivity

---
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DeviceListActivity.java
@@ -0,0 +1,13 @@
+package pl.rychlinski.damian.mobilnatelemetria;
+
+import android.app.Activity;
+
+/**
+ * This Activity appears as a dialog. It lists any paired devices and
+ * devices detected in the area after discovery. When a device is chosen
+ * by the user, the MAC address of the device is sent back to the parent
+ * Activity in the result Intent.
+ */
+public class DeviceListActivity extends Activity {
+
+}
