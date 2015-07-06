89f5817db5662ae35615a2d88a3b57c06b196866
Damian Rychliński <drychlinski@outlook.com>
2015-07-06 16:13:37 +0200

    UslugaBluetooth: dodanie konstruktora oraz stałych pól klasy

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/UslugaBluetooth.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/UslugaBluetooth.java
@@ -1,7 +1,38 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.os.Handler;
+
 /**
- * Created by damian on 06.07.15.
+ * This class does all the work for setting up and managing Bluetooth
+ * connections with other devices. It has a thread that listens for
+ * incoming connections, a thread for connecting with a device, and a
+ * thread for performing data transmissions when connected.
  */
 public class UslugaBluetooth {
+    private static final String TAG = "BluetoothChatService";
+
+    // Member fields
+    private final BluetoothAdapter mAdapter;
+    private final Handler mHandler;
+    private int mState;
+
+    // Constants that indicate the current connection state
+    public static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+    public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 3;  // now connected to a remote device
+
+    /**
+     * Constructor. Prepares a new BluetoothChat session.
+     *
+     * @param context The UI Activity Context
+     * @param handler A Handler to send messages back to the UI Activity
+     */
+    public BluetoothChatService(Context context, Handler handler) {
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mState = STATE_NONE;
+        mHandler = handler;
+    }
 }
