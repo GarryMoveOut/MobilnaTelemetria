30e05ea31dc6a67fcf629e7fa7984ad2efb5c3ba
Damian Rychliński <drychlinski@outlook.com>
2015-07-06 11:25:42 +0200

    BluetoothFragment: setStatus(String)

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -210,4 +210,20 @@ public class BluetoothFragment extends Fragment {
         }
         actionBar.setSubtitle(resId);
     }
+    /**
+     * Updates the status on the action bar.
+     *
+     * @param subTitle status
+     */
+    private void setStatus(CharSequence subTitle) {
+        FragmentActivity activity = getActivity();
+        if (null == activity) {
+            return;
+        }
+        final ActionBar actionBar = activity.getActionBar();
+        if (null == actionBar) {
+            return;
+        }
+        actionBar.setSubtitle(subTitle);
+    }
 }
