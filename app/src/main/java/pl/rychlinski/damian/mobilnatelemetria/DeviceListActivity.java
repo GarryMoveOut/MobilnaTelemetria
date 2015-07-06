e374dd0a74fe86852e703d777c10d68b51e61492
Damian Rychliński <drychlinski@outlook.com>
2015-07-06 12:11:45 +0200

    DeviceListActivity: Dodanie pól klasy

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DeviceListActivity.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DeviceListActivity.java
@@ -1,6 +1,8 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.ArrayAdapter;
 
 /**
  * This Activity appears as a dialog. It lists any paired devices and
@@ -9,5 +11,10 @@ import android.app.Activity;
  * Activity in the result Intent.
  */
 public class DeviceListActivity extends Activity {
+    private static final String TAG = "DeviceListActivity";
+
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    private BluetoothAdapter mBtAdapter;
+    private ArrayAdapter<String> mNewDevicesArrayAdapter;
 
 }
