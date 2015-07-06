0c55bcf3596fd866a735ef147472886b46259a7e
Damian Rychliński <drychlinski@outlook.com>
2015-07-06 12:05:28 +0200

    BluetoothFragment: onActivityResult

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -1,6 +1,7 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
@@ -225,5 +226,34 @@ public class BluetoothFragment extends Fragment {
             return;
         }
         actionBar.setSubtitle(subTitle);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, true);
+                }
+                break;
+            case REQUEST_CONNECT_DEVICE_INSECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, false);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    setupChat();
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Log.d(TAG, "BT not enabled");
+                    Toast.makeText(getActivity(), R.string.bt_not_enabled_leaving,
+                            Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                }
+        }
     }
 }
