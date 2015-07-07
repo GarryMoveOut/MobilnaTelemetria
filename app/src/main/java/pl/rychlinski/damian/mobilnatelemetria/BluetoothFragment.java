8b9ee5e91aad52c727a13421a17f4325b9ce0f80
Damian Rychliński <drychlinski@outlook.com>
2015-07-07 12:56:33 +0200

    BLuetoothFragment: onResume

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/BluetoothFragment.java
@@ -325,4 +325,20 @@ public class BluetoothFragment extends Fragment {
             mChatService.stop();
         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == UslugaBluetooth.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
 }
