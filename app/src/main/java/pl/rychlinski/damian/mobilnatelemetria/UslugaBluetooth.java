66359e96fc279e836483bc99d21d5bca2770cef3
Damian Rychliński <drychlinski@outlook.com>
2015-07-07 13:12:40 +0200

    UslugaBluetooth: stop()

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/UslugaBluetooth.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/UslugaBluetooth.java
@@ -352,6 +352,34 @@ public class UslugaBluetooth {
     }
 
     /**
+     * Stop all threads
+     */
+    public synchronized void stop() {
+        Log.d(TAG, "stop");
+
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        /*if (mSecureAcceptThread != null) {
+            mSecureAcceptThread.cancel();
+            mSecureAcceptThread = null;
+        }
+
+        if (mInsecureAcceptThread != null) {
+            mInsecureAcceptThread.cancel();
+            mInsecureAcceptThread = null;
+        }*/
+        setState(STATE_NONE);
+    }
+
+    /**
      * Start the chat service. Specifically start AcceptThread to begin a
      * session in listening (server) mode. Called by the Activity onResume()
      */
