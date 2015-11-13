368cba077040dac1a8510e29ee827c0d3759e584
Damian Rychliński <damian.true@hotmail.com>
2015-11-13 16:34:31 +0100

    Utworzenie ramy wątku DekoderPID

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DekoderPID.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DekoderPID.java
@@ -1,7 +1,31 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
+import android.os.Handler;
+import android.os.Message;
+
 /**
  * Created by damian on 13.11.15.
  */
 public class DekoderPID extends Thread {
+    private Handler parentHandler;
+
+    private Handler myThreadHandler = new Handler(){
+        public void handleMessage(Message msg) {
+            // ...
+        }
+    };
+
+    public DekoderPID(Handler parentHandler){
+        this.parentHandler = parentHandler;
+        // ...
+    }
+
+    @Override
+    public void run() {
+        // ...
+    }
+
+    public Handler getHandler() {
+        return myThreadHandler;
+    }
 }
