048dc71511e98bb5b6bb182bcf8dc32df09195e8
Damian Rychliński <damian.true@hotmail.com>
2016-02-09 17:11:09 +0100

    Dodanie broadcast receivera do analizatora jazdy

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DriveAnalizerService.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DriveAnalizerService.java
@@ -1,6 +1,8 @@
 package pl.rychlinski.damian.mobilnatelemetria;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
@@ -16,4 +18,17 @@ public class DriveAnalizerService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals("android.provider.Telephony.SMS_RECEIVED")){
+                //action for sms received
+            }
+            else if(action.equals(android.telephony.TelephonyManager.ACTION_PHONE_STATE_CHANGED)){
+                //action for phone state changed
+            }
+        }
+    };
 }
