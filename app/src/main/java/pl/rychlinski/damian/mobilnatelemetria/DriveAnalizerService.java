46902d03a028602726a538dd608bbdac3f710817
Damian Rychliński <damian.true@hotmail.com>
2016-02-24 22:48:43 +0100

    Implementacja algorytmu oceny jazdy

--- app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DriveAnalizerService.java
+++ app/src/main/java/pl/rychlinski/damian/mobilnatelemetria/DriveAnalizerService.java
@@ -11,8 +11,31 @@ import android.util.Log;
 public class DriveAnalizerService extends Service {
     private static final String TAG = "DriveAnalizerService";
 
+    private float driveMark;
+
+    //progi warunków
+    int w0, w1L, w1Pl, w1Pg, w2L, w2P, w3, w5;
+    float w4;
+
+    //kary
+    float sfrf, sfrtcf, sfrtct,
+            strf, strtcf, strtct,
+            tf, tt,
+            gf, gt,
+            lf,lt;
+
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
+        w0 = 5;
+        w1L = 1200;
+        w1Pl = 1800;
+        w1Pg = 3500;
+        w2L = 80;
+        w2P = 80;
+        w3 = 50;
+        w4 = 0.25f;
+        w5 = 0; //?
 
         return Service.START_STICKY;
     }
@@ -24,6 +47,8 @@ public class DriveAnalizerService extends Service {
 
     @Override
     public void onCreate() {
+        driveMark = 0;
+
         IntentFilter filter = new IntentFilter();
         filter.addAction("pl.rychlinski.damian.mobilnatelemetria.pid.rpm");
         filter.addAction("pl.rychlinski.damian.mobilnatelemetria.pid.load");
@@ -41,44 +66,100 @@ public class DriveAnalizerService extends Service {
     }
 
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        boolean rpmChk, loadChk, coolantTempChk, speedChk, airtempChk, throttleChk;
+        float rpm;
+        float load;
+        int coolanttemp;
+        int speed;
+        int airtemp;
+        float throttle;
+        float gforce;
+
+
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
 
             if(action.equals("pl.rychlinski.damian.mobilnatelemetria.pid.rpm")){
                 Log.d(TAG,"Received RPM");
-                float rpm = intent.getExtras().getFloat("RPM");
-                float tmp = rpm - 1f; //TODO: Do usunięcia po skączeniu testów
+                rpm = intent.getExtras().getFloat("RPM");
+                if(rpm > 0f && rpm < 17000f) rpmChk = true;
             }
 
             if(action.equals("pl.rychlinski.damian.mobilnatelemetria.pid.load")){
                 Log.d(TAG,"Received Load");
-                float load = intent.getExtras().getFloat("LOAD");
-                float tmp = load - 1f; //TODO: Do usunięcia po skończeniu testów
+                load = intent.getExtras().getFloat("LOAD");
+                if(load > 0f && load < 100f) loadChk = true;
             }
 
             if(action.equals("pl.rychlinski.damian.mobilnatelemetria.pid.coolanttemp")){
                 Log.d(TAG,"Received Coolanttemp");
-                float coolanttemp = intent.getExtras().getInt("COOLANTTEMP");
-                float tmp = coolanttemp - 1f; //TODO: Do usunięcia po skończeniu testów
+                coolanttemp = intent.getExtras().getInt("COOLANTTEMP");
+                if(coolanttemp > -40 && coolanttemp < 215) coolantTempChk = true;
             }
 
             if(action.equals("pl.rychlinski.damian.mobilnatelemetria.pid.speed")){
                 Log.d(TAG,"Received Speed");
-                float speed = intent.getExtras().getInt("SPEED");
-                float tmp = speed - 1f; //TODO: Do usunięcia po skończeniu testów
+                speed = intent.getExtras().getInt("SPEED");
+                if(speed > 0 && speed < 255) speedChk = true;
             }
 
             if(action.equals("pl.rychlinski.damian.mobilnatelemetria.pid.airtemp")){
                 Log.d(TAG,"Received Airspeed");
-                int airtemp = intent.getExtras().getInt("AIRTEMP");
-                float tmp = airtemp - 1f; //TODO: Do usunięcia po skończeniu testów
+                airtemp = intent.getExtras().getInt("AIRTEMP");
+                if(airtemp > -40 && airtemp < 215) airtempChk = true;
             }
 
             if(action.equals("pl.rychlinski.damian.mobilnatelemetria.pid.throttle")){
                 Log.d(TAG,"Received throttle");
-                int throttle = intent.getExtras().getInt("THROTTLE");
-                float tmp = throttle - 1f; //TODO: Do usunięcia po skończeniu testów
+                throttle = intent.getExtras().getFloat("THROTTLE");
+                if(throttle > 0f && throttle < 100f) throttleChk = true;
+            }
+
+            if(rpmChk && loadChk && coolantTempChk && speedChk && airtempChk && throttleChk){
+                //TODO wczytać gforce
+                if(speed > w0){
+                    if(rpm < w1Pl && rpm > w1Pg){
+                        if(coolanttemp < w2P){
+                            driveMark =+ (rpm/100)*strtct; //kara x2
+                        }else{
+                            driveMark =- (rpm/100)*strtcf; //kara
+                        }
+                    }else{
+                        driveMark =- 1 * strf; //nagroda
+                    }
+                }else{
+                    if(rpm > w1L){
+                        if(coolanttemp < w2L){
+                            driveMark =+ (rpm/100)*sfrtct; //kara x2
+                        }else{
+                            driveMark =+ (rpm/100)*sfrtcf; //kara
+                        }
+                    }else{
+                        driveMark =- 1*sfrf; //nagroda
+                    }
+                }
+
+//czesc 2
+
+                if(throttle > w3){
+                    driveMark =+ throttle * tt; //kara
+                }else{
+                    driveMark =- (100 - throttle) * tf; //nagroda
+                }
+
+                if(gforce > w4){
+                    driveMark =+ gforce*100*gt; //kara
+                    driveMark =- 100-(gforce*100) * gf; //nagroda
+                }else{
+                }
+
+                if(load > w5){
+                    //kara
+                }else{
+                    //nagroda
+                }
             }
         }
     };
